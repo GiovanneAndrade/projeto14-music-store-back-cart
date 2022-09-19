1bef4ddac1a0da7cfc112affc7a01e9a63008a10
GiovanneAndrade <vanne.as@hotmail.com>
2022-09-19 15:15:47 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { authRouter } from './routes/auth.routes.js';
 //import { cartRouter } from './routes/cart.routes.js';
-
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,4 +11,4 @@ app.use(authRouter);
 //app.use(cartRouter);
 
 
-app.listen(5000, () => console.log("Ouvindo"));
+app.listen(port, () => console.log("Ouvindo"));
